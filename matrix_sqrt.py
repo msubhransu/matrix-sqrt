46814560d8fd7c64bd5063e8aa735d9f2068b53b
Florian Will <florian.will@tum.de>
2018-09-13 16:34:21 +0200

    Execute script only when run as main file.

--- matrix_sqrt.py
+++ matrix_sqrt.py
@@ -110,83 +110,89 @@ def create_symm_matrix(batchSize, dim, numPts, tau, dtype):
     print('Creating batch %d, dim %d, pts %d, tau %f, dtype %s' %(batchSize, dim, numPts, tau, dtype))
     return A
 
-# Command line arguments
-parser = argparse.ArgumentParser(description='Matrix squareroot and its gradient demo')
-parser.add_argument('--batch-size', type=int, default=128, metavar='N',
-                    help='input batch size for training (default: 128)')
-parser.add_argument('--pts', type=int, default=1000, metavar='N',
-                    help='number of points to construct covariance matrix (default: 1000)')
-parser.add_argument('--tau', type=float, default=1.0, metavar='N',
-                    help='conditioning by adding to the diagonal (default: 1.0)')
-parser.add_argument('--num-iters', type=int, default=10, metavar='N',
-                    help='number of schulz iterations (default: 5)')
-parser.add_argument('--dim', type=int, default=64, metavar='N',
-                    help='size of the covariance matrix (default: 64)')
-parser.add_argument('--cuda', action='store_true', default=False,
-                    help='disables CUDA training')
-args = parser.parse_args()
-if args.cuda:
-  d = torch.cuda.FloatTensor
-else:
-  d = torch.FloatTensor
+def evaluate_performance():
+    # Command line arguments
+    parser = argparse.ArgumentParser(description='Matrix squareroot and its gradient demo')
+    parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+                        help='input batch size for training (default: 128)')
+    parser.add_argument('--pts', type=int, default=1000, metavar='N',
+                        help='number of points to construct covariance matrix (default: 1000)')
+    parser.add_argument('--tau', type=float, default=1.0, metavar='N',
+                        help='conditioning by adding to the diagonal (default: 1.0)')
+    parser.add_argument('--num-iters', type=int, default=10, metavar='N',
+                        help='number of schulz iterations (default: 5)')
+    parser.add_argument('--dim', type=int, default=64, metavar='N',
+                        help='size of the covariance matrix (default: 64)')
+    parser.add_argument('--cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    args = parser.parse_args()
+    if args.cuda:
+      d = torch.cuda.FloatTensor
+    else:
+      d = torch.FloatTensor
 
-# Create matrix and gradient randomly
-A = Variable(create_symm_matrix(batchSize=args.batch_size, dim=args.dim, numPts=args.pts, tau=args.tau, dtype=d), requires_grad=True)
-dldz = Variable(torch.randn(args.batch_size, args.dim, args.dim).type(d), requires_grad=False)
-dldz = 0.5*(dldz + dldz.transpose(1,2));
+    # Create matrix and gradient randomly
+    A = Variable(create_symm_matrix(batchSize=args.batch_size, dim=args.dim, numPts=args.pts, tau=args.tau, dtype=d), requires_grad=True)
+    dldz = Variable(torch.randn(args.batch_size, args.dim, args.dim).type(d), requires_grad=False)
+    dldz = 0.5*(dldz + dldz.transpose(1,2));
 
-# Forward + backward with SVD
-# Time: O(n^3), Space: O(n^3)
-print('Singular Value Decomposition (SVD):')
-start=tm.time()
-svd_sA, svd_grad, svd_error = sqrt_svd_lyap(A, -dldz, dtype=d)
-end=tm.time()
-svd_time = end-start;
-print('  >> forward + backward time %fs, forward error %s' % (svd_time, svd_error.item()))
+    # Forward + backward with SVD
+    # Time: O(n^3), Space: O(n^3)
+    print('Singular Value Decomposition (SVD):')
+    start=tm.time()
+    svd_sA, svd_grad, svd_error = sqrt_svd_lyap(A, -dldz, dtype=d)
+    end=tm.time()
+    svd_time = end-start;
+    print('  >> forward + backward time %fs, forward error %s' % (svd_time, svd_error.item()))
 
 
-# Forward pass with Denman-Beavers iterations (no backward)
-print('Denman-Beavers iterations (%i iters) ' % (args.num_iters))
-start = tm.time()
-sA, error = sqrt_denman_beavers(A, args.num_iters, dtype=d);
-end = tm.time()
-print('  >> forward time %fs, error %s' % (end-start, error.item()))
-print('  >> no backward via autograd')
+    # Forward pass with Denman-Beavers iterations (no backward)
+    print('Denman-Beavers iterations (%i iters) ' % (args.num_iters))
+    start = tm.time()
+    sA, error = sqrt_denman_beavers(A, args.num_iters, dtype=d);
+    end = tm.time()
+    print('  >> forward time %fs, error %s' % (end-start, error.item()))
+    print('  >> no backward via autograd')
 
-# Forward pass with Newton-Schulz (autograd version)
-# Time: O(Tn^2), Space: O(Tn^2), with T iterations
-print('Newton-Schulz iterations (%i iters) ' % (args.num_iters))
-start = tm.time()
-sA, error = sqrt_newton_schulz_autograd(A, args.num_iters, dtype=d);
-end = tm.time()
-iter_time = end-start;
-print('  >> forward: time %fs, error %s' % (end-start, error.item()))
+    # Forward pass with Newton-Schulz (autograd version)
+    # Time: O(Tn^2), Space: O(Tn^2), with T iterations
+    print('Newton-Schulz iterations (%i iters) ' % (args.num_iters))
+    start = tm.time()
+    sA, error = sqrt_newton_schulz_autograd(A, args.num_iters, dtype=d);
+    end = tm.time()
+    iter_time = end-start;
+    print('  >> forward: time %fs, error %s' % (end-start, error.item()))
 
-# Backward pass with autograd
-start = tm.time()
-#with torch.autograd.profiler.profile() as prof:
-sA.backward(dldz)
-#print(prof)
-end = tm.time()
-iter_time += end-start
-backward_error = svd_grad.dist(A.grad.data)
-print('  >> backward via autograd: time %fs, error %f' % (end-start, backward_error))
-print('  >> speedup over SVD: %.1fx' %(svd_time / iter_time))
+    # Backward pass with autograd
+    start = tm.time()
+    #with torch.autograd.profiler.profile() as prof:
+    sA.backward(dldz)
+    #print(prof)
+    end = tm.time()
+    iter_time += end-start
+    backward_error = svd_grad.dist(A.grad.data)
+    print('  >> backward via autograd: time %fs, error %f' % (end-start, backward_error))
+    print('  >> speedup over SVD: %.1fx' %(svd_time / iter_time))
 
-# Forward pass with Newton-Schulz
-# Time: O(Tn^2), Space: O(n^2), with T iterations
-print('Newton-Schulz iterations (foward + backward) (%i iters) ' % (args.num_iters))
-start = tm.time()
-sA, error = sqrt_newton_schulz(A.data, args.num_iters, dtype=d);
-end = tm.time()
-iter_time = end-start
-print('  >> forward: time %fs, error %s' % (end-start, error))
+    # Forward pass with Newton-Schulz
+    # Time: O(Tn^2), Space: O(n^2), with T iterations
+    print('Newton-Schulz iterations (foward + backward) (%i iters) ' % (args.num_iters))
+    start = tm.time()
+    sA, error = sqrt_newton_schulz(A.data, args.num_iters, dtype=d);
+    end = tm.time()
+    iter_time = end-start
+    print('  >> forward: time %fs, error %s' % (end-start, error))
 
-# Backward pass with Newton-Schulz
-start = tm.time()
-dlda = lyap_newton_schulz(sA, dldz.data, args.num_iters, dtype=d)
-end = tm.time()
-iter_time += end-start
-backward_error = svd_grad.dist(dlda)
-print('  >> backward: time %fs, error %f ' % (end-start, backward_error))
-print('  >> speedup over SVD: %.1fx' %(svd_time / iter_time))
+    # Backward pass with Newton-Schulz
+    start = tm.time()
+    dlda = lyap_newton_schulz(sA, dldz.data, args.num_iters, dtype=d)
+    end = tm.time()
+    iter_time += end-start
+    backward_error = svd_grad.dist(dlda)
+    print('  >> backward: time %fs, error %f ' % (end-start, backward_error))
+    print('  >> speedup over SVD: %.1fx' %(svd_time / iter_time))
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    evaluate_performance()
